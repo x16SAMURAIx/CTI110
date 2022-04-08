03609d0f3d629d1d8a7dac492d227955437d0dfc
x16SAMURAIx <103261056+x16SAMURAIx@users.noreply.github.com>
2022-04-08 09:22:25 -0400

    Update P1LAB1_KeeslerJordan.py

--- P1LAB1_KeeslerJordan.py
+++ P1LAB1_KeeslerJordan.py
@@ -1,8 +1,6 @@
 userNum = int(input())
 userNumSquared = userNum * userNum
 
-# Bug here; fix it when instructed
    
 print(userNumSquared) 
 
-# Output formatting issue here; fix it when instructed
