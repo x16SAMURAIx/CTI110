aedb26a5553eb180f82df8f762e5b90d5a3cb6e0
x16SAMURAIx <103261056+x16SAMURAIx@users.noreply.github.com>
2022-04-08 12:03:33 -0400

    Add files via upload
    Pizza calculator sub1

---
+++ P1HW2_KeeslerJordan.py
@@ -0,0 +1,22 @@
+#Pizza order program
+#08 APRIL 2022
+#CTI-110 P1HW2 - Pizza Order
+#Jordan Keesler
+#
+
+
+print('How many students do you want to order pizza for?', end=' ')
+num1 = int(input())
+num2 = 3
+num3 = 6
+print('----Pizza Order--------')
+
+print('Number of Students:', num1)
+
+print('Number of Slices:', num1 * num2)
+
+print('Number of Pizzas:', num1 * num2 / num3)
+
+print('-----------------------')
+
+
