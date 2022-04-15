1a96919bd1435ebfc71f6fb7cff8526d481cf25f
x16SAMURAIx <103261056+x16SAMURAIx@users.noreply.github.com>
2022-04-15 09:19:04 -0400

    Add files via upload
    P2LAB1

---
+++ P2LAB1_KeeslerJordan.py
@@ -0,0 +1,14 @@
+user_int = int(input('Enter integer (32 - 126):\n'))
+floatinput = float(input('Enter float:\n'))
+charinput = input('Enter character:\n')
+strInput = input('Enter string:\n')
+print(user_int,floatinput,charinput,strInput)
+print(strInput,charinput,floatinput,user_int)
+print(user_int,'converted to a character is',chr(user_int))
+
+
+# FIXME (1): Finish reading other items into variables, then output the four values on a single line separated by a space
+   
+# FIXME (2): Output the four values in reverse
+   
+# FIXME (3): Convert the integer to a characer, and output that character
